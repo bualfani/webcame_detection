c202575f28763f5901172f45abdc318b4a6ffa9b
bahati <bulozealfani@gmail.com>
2023-03-30 20:51:20 -0500

    adding email where image would be sent

--- emailing.py
+++ emailing.py
@@ -1,5 +1,17 @@
+import imghdr
 import smtplib
+from email.message import EmailMessage
 
+# add google password received from generating one
 password = "your_password"
 
-def send_email():
+def send_email(img_path):
+    email_message = EmailMessage
+    email_message['Subject'] = 'New Customer'
+    email_message.set_content('Hey, a new customer is here')
+
+    with open(img_path, 'rb') as file:
+        content = file.read()
+    email_message.add_attachment(content, maintype='image', subtype=imghdr.what(None, content))
+
+
